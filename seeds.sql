2264f30dcde3f40dd6c4d67b19a344b32f3e53a3
KHulsy <66654951+KHulsy@users.noreply.github.com>
2020-07-22 23:10:04 -0700

    Create seeds.sql
    Herein lieth the seeds.

---
+++ seeds.sql
@@ -0,0 +1,22 @@
+DROP DATABASE IF EXISTS;
+CREATE -employees_DB;
+USE    employees_DB;
+CREATE TABLE employees(
+id int NOT NULL AUTO INCREMENT,
+employees VARCHAR(50)
+PRIMARY KEY (id)
+);
+INSERT INTO employees(employees) VALUE("george castanza celebrates festivus");
+INSERT INTO employees(employees) VALUE("elaine bennis eats antiquated cake");
+INSERT INTO employees(employees) VALUE("cosmo kramer fell asleep in a tanning bed";
+INSERT INTO employees(employees) VALUE("jerry seinfeld is an anti dentite");
+INSERT INTO employees(employees) VALUE("george castanza schmoopy schmoopy");
+INSERT INTO employees(employees) VALUE("newman loves jumbilyah");
+INSERT INTO employees(employees) VALUE("jerry seinfeld loved rochelle rochelle")
+INSERT INTO employees(employees) VALUE ("jerry seinfeld did not love prognosis negative";
+INSERT INTO employees(employees) VALUE ("george castanza must fight his father");
+INSERT INTO employees(employees) VALUE ("elaine bennis does not support the urban sombrero");
+INSERT INTO employees(employees) VALUE ("kramer won the contest");
+INSERT INTO employees(employees) VALUE ("elaine bennis said maybe the dingo ate your baby");
+INSERT INTO employees(employees) VALUE ("the soup nazi says no soup for you"); 
+IF NOT EXISTS(employees),THEN FALSE;
