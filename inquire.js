5318db6aae1e3dfa385ba216534bf76c9eeac98d
KHulsy <66654951+KHulsy@users.noreply.github.com>
2020-07-22 20:01:49 -0700

    Create inquire.js
    Code from command line: raw list.

---
+++ inquire.js
@@ -0,0 +1,78 @@
+kaitline.hulsy@Kaitlins-Air-2 ucb-ber-fsf-ft-06-2020-u-c % var prompt=inquirer.createPromptModule();
+function> prompt(questions).then(* ... */;
+kaitline.hulsy@Kaitlins-Air-2 ucb-ber-fsf-ft-06-2020-u-c % const inquirer=require('inquirer');
+zsh: unknown file attribute: i
+kaitline.hulsy@Kaitlins-Air-2 ucb-ber-fsf-ft-06-2020-u-c % inquirer.prompt([
+> type: 'rawlist',
+> name: employees
+> name: 'employees'
+> message: 'Which employee would you like to access?'
+> choices: ['Prince', 'Freddy Mercury', 'Stevie Nicks', 'Fiona Apple'],
+> },
+> })
+zsh: invalid subscript
+kaitline.hulsy@Kaitlins-Air-2 ucb-ber-fsf-ft-06-2020-u-c % ])
+zsh: parse error near `)'
+kaitline.hulsy@Kaitlins-Air-2 ucb-ber-fsf-ft-06-2020-u-c % ]).then(answers=>{ console.info('Answer: 'Prince', answers.prince);  
+zsh: parse error near `)'
+kaitline.hulsy@Kaitlins-Air-2 ucb-ber-fsf-ft-06-2020-u-c % });
+zsh: parse error near `}'
+kaitline.hulsy@Kaitlins-Air-2 ucb-ber-fsf-ft-06-2020-u-c % var prompt=inquirer.createPromptModule();
+function> prompt(questions).then(/* ...*/);
+kaitline.hulsy@Kaitlins-Air-2 ucb-ber-fsf-ft-06-2020-u-c % const inquirer=require('inquirer');
+zsh: unknown file attribute: i
+kaitline.hulsy@Kaitlins-Air-2 ucb-ber-fsf-ft-06-2020-u-c % inquirer.prompt([ { type:'rawlist',
+> name:'sales people'
+> message: 'Who do you want to talk to?',
+> choices['bizarro jerry', 'jerry'],
+> },
+> ])
+zsh: bad math expression: ':' without '?'
+zsh: bad math expression: operand expected at end of string
+kaitline.hulsy@Kaitlins-Air-2 ucb-ber-fsf-ft-06-2020-u-c % .then(answers =>{
+zsh: bad pattern: .then(answers =
+kaitline.hulsy@Kaitlins-Air-2 ucb-ber-fsf-ft-06-2020-u-c % console.info('Answer:'bizarro jerry');
+quote> });
+quote> var prompt=inquirer.createPromptModule();
+quote> prompt(questions).then(/* ... */);
+quote> const inquirer=require('inquire');
+quote> inquirer.prompt([{ type: 'rawlist', name: 'salaries',message: 'Tell me how much the Jerrys make.'
+quote> choices:[ '5,000,000', '10,000,000']},
+quote> ])
+quote> .then(answers => {
+quote> console.info('Answer: 5,000,000',answers.5,000,000);)
+quote> });
+quote> var prompt=inquirer.createPromptModule();
+quote> inquirer
+quote> .prompt([
+quote> {
+quote> type: 'rawlist',
+quote> name: 'readme',
+quote> message: 'Which is better?',
+quote> choices: ['professional', 'fabulous'],
+quote> inquirer.prompt([ { type:'rawlist',
+name:'sales people'
+message: 'Who do you want to talk to?',
+choices['bizarro jerry', 'jerry'],
+},
+]).then(answers =>{
+quote> console.info('Answer: 'bizarro jerry', answers.bizarrojerry);
+zsh: unknown sort specifier
+kaitline.hulsy@Kaitlins-Air-2 ucb-ber-fsf-ft-06-2020-u-c % });
+zsh: parse error near `}'
+kaitline.hulsy@Kaitlins-Air-2 ucb-ber-fsf-ft-06-2020-u-c % var prompt=inquirer.createPromptModule();
+function> prompt(questions).then(/* ...*/);
+kaitline.hulsy@Kaitlins-Air-2 ucb-ber-fsf-ft-06-2020-u-c % const inquirer=require('inquirer');
+zsh: unknown file attribute: i
+kaitline.hulsy@Kaitlins-Air-2 ucb-ber-fsf-ft-06-2020-u-c % inquirer.prompt([
+> type: 'rawlist',
+> name: 'firing',
+> message: 'Who are you firing?'
+> choices: ['the soup nazi', 'mr. peterman'],
+> },
+> ])
+zsh: bad math expression: ':' without '?'
+zsh: bad math expression: ':' without '?'
+kaitline.hulsy@Kaitlins-Air-2 ucb-ber-fsf-ft-06-2020-u-c % .then(answers => {
+zsh: bad pattern: .then(answers =
+kaitline.hulsy@Kaitlins-Air-2 ucb-ber-fsf-ft-06-2020-u-c % console.info('Answer: 'mr peterman', answers.mrpeterman);
